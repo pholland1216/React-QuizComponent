8d5456f25661d74c027d46c17fe04d53f1921d71
Phillip Holland <phillip.holland@disa.com>
2021-01-25 17:07:57 -0600

    Added button click and next question handling.

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,13 +10,19 @@ class Quiz extends Component {
         this.state = {quiz_position: 1};
     }
     
+    showNextQuestion(){
+        this.setState({quiz_position: this.state.quiz_position + 1})
+    }
+
     render(){
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
         
         if (isQuizEnd) {
             return <QuizEnd />
         } else {
-            return <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}/>
+            return <QuizQuestion
+                        quiz_question={quizData.quiz_questions[this.state.quiz_position-1]}
+                        showNextQuestionHandler={this.showNextQuestion.bind(this)} />
         }
     }
 }
